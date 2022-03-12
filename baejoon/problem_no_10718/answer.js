7373bdb1c87280c829cd619db88b98e5513ab522
hcs1105 <hcs1105@gmail.com>
2022-03-13 00:35:00 +0900

    10718번 문제 풀이

---
+++ baejoon/problem_no_10718/answer.js
@@ -0,0 +1,19 @@
+// 10718번 문제 - https://www.acmicpc.net/problem/10718
+
+// 나의 풀이
+console.log('강한친구 대한육군');
+console.log('강한친구 대한육군');
+
+// aooen님의 풀이
+console.log((a='강한친구 대한육군\n')+a);
+
+// zintaho님의 풀이
+console.log('강한친구 대한육군\n'.repeat(2));
+
+// gndndn02님의 풀이
+for(i=0; i<2; i++){
+	console.log("강한친구 대한육군");
+}
+
+// woowong님의 풀이
+console.log("강한친구 대한육군\n강한친구 대한육군");
