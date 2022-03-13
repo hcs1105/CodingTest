0525625ef3e5ddc56a0aae7b6b236aab9f22c77e
hcs1105 <hcs1105@gmail.com>
2022-03-13 21:39:55 +0900

    10869번 문제 풀이

---
+++ baekjoon/problem_no_10869/answer.js
@@ -0,0 +1,18 @@
+// 10869번 문제 - https://www.acmicpc.net/problem/10869WQ
+
+const fs = require('fs');
+const filePath = process.platform === 'linux' ?  '/dev/stdin' : './data.txt';
+let input = fs.readFileSync(filePath).toString().split('\n');
+
+input = input[0];
+input = input.split(' ').map(item => +item);
+
+function solution(a,b){
+  console.log(a + b);
+  console.log(a - b);
+  console.log(a * b);
+  console.log(Math.floor(a / b));
+  console.log(a % b);
+}
+
+solution(input[0], input[1]);
