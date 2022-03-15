401d3797c7ad3e055792b727803f9d96ab102d77
hcs1105 <hcs1105@gmail.com>
2022-03-15 13:25:22 +0900

    9488번 문제 풀이

---
+++ baekjoon/problem_9488/answer.js
@@ -0,0 +1,21 @@
+// 9488번 문제 - https://www.acmicpc.net/problem/9488
+
+const fs = require('fs');
+const filePath = process.platform === 'linux' ?  '/dev/stdin' : './data.txt';
+let input = fs.readFileSync(filePath).toString().split('\n');
+
+function solution(score){
+  if (90 <= score && score <= 100) {
+    console.log('A');
+  } else if (80 <= score && score <= 89) {
+    console.log('B');
+  } else if (70 <= score && score <= 79) {
+    console.log('C');
+  } else if (60 <= score && score <= 69) {
+    console.log('D');
+  } else {
+    console.log('F');
+  }
+}
+
+solution(+input[0]);
