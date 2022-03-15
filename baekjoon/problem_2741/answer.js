8589892e8adfea438ec7eb6d2022c8bbfc3eb040
hcs1105 <hcs1105@gmail.com>
2022-03-16 00:43:48 +0900

    2741번 문제 풀이에 적용된 코드 리팩토링

--- baekjoon/problem_2741/answer.js
+++ baekjoon/problem_2741/answer.js
@@ -4,7 +4,7 @@ const fs = require('fs');;
 const filePath = process.platform === 'linux' ?  '/dev/stdin' : './data.txt';
 let input = fs.readFileSync(filePath).toString().split('\n');
 
-const inputNum = Number(input[0]);
+const inputNum = +input[0];
 let answer = '';
 
 for (let i = 1; i <= inputNum; i++) {
