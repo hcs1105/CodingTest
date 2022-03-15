1e36d82ee5675b6e96e63f6341f81bbb1920d870
hcs1105 <hcs1105@gmail.com>
2022-03-15 12:10:25 +0900

    2588번 문제 풀이

--- baekjoon/problem_no_2588/answer.js
+++ baekjoon/problem_no_2588/answer.js
@@ -1,4 +1,4 @@
-// 10869번 문제 - https://www.acmicpc.net/problem/10869WQ
+// 2588번 문제 - https://www.acmicpc.net/problem/2588
 
 const fs = require('fs');
 const filePath = process.platform === 'linux' ?  '/dev/stdin' : './data.txt';
@@ -7,7 +7,14 @@ let input = fs.readFileSync(filePath).toString().split('\n');
 input = input.map(item => +item);
 
 function solution(a,b){
-  console.log(a, b);
+  const charB = String(b);
+  const digitalOne = +charB[2];
+  const digitalTwo = +charB[1];
+  const digitalThree = +charB[0];
+
+  console.log(a * digitalOne);
+  console.log(a * digitalTwo);
+  console.log(a * digitalThree);
   console.log(a * b);
 }
 
