d01a3b70b69dbe8f1622f94e3c23b7c879dbbe93
hcs1105 <hcs1105@gmail.com>
2022-03-16 00:03:47 +0900

    2741번 문제 풀이

---
+++ baekjoon/problem_2741/answer.js
@@ -0,0 +1,14 @@
+// 2741번 문제 - https://www.acmicpc.net/problem/2741
+
+const fs = require('fs');;
+const filePath = process.platform === 'linux' ?  '/dev/stdin' : './data.txt';
+let input = fs.readFileSync(filePath).toString().split('\n');
+
+const inputNum = Number(input[0]);
+let answer = '';
+
+for (let i = 1; i <= inputNum; i++) {
+    answer += i + "\n";
+}
+
+console.log(answer);
